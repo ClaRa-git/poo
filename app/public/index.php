a7323297c1553f5ac390c74d3c181937fb8f8fd8
ClaRa-git <rameau.claire.cr@gmail.com>
2024-11-22 13:44:37 +0100

    Mise en place du Singleton

--- app/public/index.php
+++ app/public/index.php
@@ -6,6 +6,14 @@ require_once '../vendor/autoload.php';
 // Déclaration de l'espace de nom des classes que l'on va utiliser
 use App\App;
 
-$truc = new App();
+var_dump( App::getApp() );
 
-$truc->toto(); // Affiche "Je suis Toto"
+$truc = App::getApp();
+$machin = clone $truc;
+
+$truc->toto( "Truc a dit" );
+$truc->toto( "Et ensuite Truc a dit" );
+$machin->toto( "Machin a dit" );
+
+var_dump( $truc );
+var_dump( $machin );
--- app/src/App.php
+++ app/src/App.php
@@ -8,9 +8,41 @@ namespace App;
 
 class App
 {
-    public function toto( ): void
+    // Singleton étape 2 : on crée une propriété statique pour stocker l'instance unique
+    // self → fait référence à la classe courante (App)
+    // ? devant "self" pour autoriser la valeur "null"
+    private static ?self $app_instance = null;
+    private string $last_message;
+
+    // Singleton étape 3 : on crée une méthode publique statique qui permet d'obtenir l'instance unique
+    public static function getApp(): self
     {
-        'Je suis Toto !';
+        // Si l'instance n'existe pas, on la crée
+        if( is_null( self::$app_instance ) )
+        {
+            self::$app_instance = new self();
+        }
+
+        return self::$app_instance;
+    }
+    public function toto( string $msg ): void
+    {
+        $this->last_message = $msg;
+        echo $msg . ' Je suis Toto !';
     }
 
+    // Singleton étape 1 : bloquer l'utilisation de new depuis l'extérieur
+    // → passer la méthode __construct (le constructeur) en "private"
+    private function __construct(){ }
+
+    // Singleton étape 4 : bloquer le clonage de l'instance
+    // → passer la méthode __clone en "private"
+    private function __clone(){ }
+
+    // Singleton étape 5 : bloquer la désérialisation de l'instance
+    // private est interdit dans ce cas, on va lui faire renvoyer une exception
+    public function __wakeup()
+    {
+        throw new \Exception( "Impossible de désérialiser une instance de " . __CLASS__ );
+    }
 }
