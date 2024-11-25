4b9dc1faf1ed18f1115f4c4bb04cbf6aae4e2002
ClaRa-git <rameau.claire.cr@gmail.com>
2024-11-25 10:53:05 +0100

    Retrait des logs

--- app/public/index.php
+++ app/public/index.php
@@ -4,15 +4,25 @@
 require_once '../vendor/autoload.php';
 
 use MiladRahimi\PhpRouter\Router;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
+use Laminas\Diactoros\Response\HtmlResponse;
 
 $router = Router::create();
 
-$router->get('/', function () {
+$router->get( '/', function () {
     return 'Bonjour page d\'accueil';
 });
 
-$router->get('/contact', function () {
+$router->get( '/contact', function () {
     return 'Bonjour page de contact';
 });
 
-$router->dispatch();
+try {
+    $router->dispatch();
+} catch ( RouteNotFoundException $e ) { // Page 404 avec statut HTTP adéquat pour les pages non listées dans les routes
+    http_response_code( 404 );
+    echo '404 - Not found';
+} catch ( Throwable $e ) { // Erreur 500 avec statut HTTP adéquat pour tout autre problème temporaire ou non
+    http_response_code( 500 );
+    echo '500 - Internal server error';
+}
