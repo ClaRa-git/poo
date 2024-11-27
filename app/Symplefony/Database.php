fcd127db9a8bf595e4b0f55825a91d8a71b4512e
ClaRa-git <rameau.claire.cr@gmail.com>
2024-11-27 12:10:28 +0100

    Modification de Database et ajout de .env

--- app/Symplefony/Database.php
+++ app/Symplefony/Database.php
@@ -3,33 +3,42 @@
 namespace Symplefony;
 
 use Exception;
+use PDO;
 
 class Database
 {
+    private const PDO_OPTIONS = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+
     // Singleton étape 2 : on crée une propriété statique pour stocker l'instance unique
-    private static ?self $db_instance = null;
+    private static ?PDO $pdo_instance = null;
 
     // Singleton étape 3 : on crée une méthode publique statique qui permet d'obtenir l'instance unique
-    public static function getDb(): self
+    public static function getPDO(): PDO
     {
         // Si l'instance n'existe pas, on la crée
-        if( is_null( self::$db_instance ) )
-        {
-            self::$db_instance = new self();
+        if (is_null(self::$pdo_instance)) {
+            $dsn = sprintf(
+                'mysql:host=%s;dbname=%s',
+                $_ENV['db_host'],
+                $_ENV['db_name']
+            );
+            self::$pdo_instance = new PDO($dsn, $_ENV['db_user'], $_ENV['db_pass'], self::PDO_OPTIONS);
         }
 
-        return self::$db_instance;
+        return self::$pdo_instance;
     }
 
     // Singleton étape 1 : bloquer l'utilisation de new depuis l'extérieur
-    private function __construct(){ }
+    private function __construct() {}
 
     // Singleton étape 4 : bloquer le clonage de l'instance
-    private function __clone(){ }
+    private function __clone() {}
 
     // Singleton étape 5 : bloquer la désérialisation de l'instance
     public function __wakeup()
     {
-        throw new Exception( "Impossible de désérialiser une instance de " . __CLASS__ );
+        throw new Exception("Impossible de désérialiser une instance de " . __CLASS__);
     }
 }
--- app/public/index.php
+++ app/public/index.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\App;
+use Dotenv\Dotenv;
 
 const DS = DIRECTORY_SEPARATOR;
-define( 'ROOT_PATH' , dirname(__FILE__, 2) . DS ); // Chemin physique du dossier racine
-define( 'APP_PATH' , ROOT_PATH . 'src' . DS ); // Chemin physique du dossier src 
+define('ROOT_PATH', dirname(__FILE__, 2) . DS); // Chemin physique du dossier racine
+define('APP_PATH', ROOT_PATH . 'src' . DS); // Chemin physique du dossier src 
 
 // Chargement de l'autoloader de Composer
-require_once ROOT_PATH .'vendor'. DS .'autoload.php';
+require_once ROOT_PATH . 'vendor' . DS . 'autoload.php';
+
+// Chargement du module .env
+Dotenv::createImmutable(ROOT_PATH)->load();
 
 // Démarrage de l'application
 App::getApp()->start();
